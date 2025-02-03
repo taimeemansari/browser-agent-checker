b037b29d164070768fd033bb7816d359b588762c
taimeem ansari <taimeemansari.n@gmail.com>
2025-02-03 18:00:29 +0530

    is ff focus check

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 async function copyToClipBoard() {
-  const toast = document.querySelector("#toast");
-  const content = document.querySelector("#user-agent-code-block").innerHTML;
+  const userAgentToast = document.querySelector("#userAgentToast");
+  const content = document.querySelector("#userAgentBlock").innerHTML;
   await navigator.clipboard.writeText(content);
-  toast.classList.remove("hidden");
+  userAgentToast.classList.remove("hidden");
   setTimeout(() => {
-    toast.classList.add("hidden");
+    userAgentToast.classList.add("hidden");
+  }, 1000);
+}
+
+function isFxFocusCheck() {
+  let userAgent = navigator.userAgent.toLowerCase();
+  const isFxFocusToast = document.querySelector("#isFxFocusToast");
+  const isFFiOs = !!userAgent.match(/fxios/i) && userAgent.match(/safari/i) === null;
+  const isFFAndriod = !!userAgent.match(/android/i) && !!userAgent.match(/firefox/i);
+  const isServiceWorkerSupported = `${"serviceWorker" in navigator}`;
+
+  if ((isFFiOs || isFFAndriod) && isServiceWorkerSupported) {
+    alert(1);
+    isFxFocusToast.innerHTML = "yay! its firefox focus browser";
+    isFxFocusToast.classList.remove("hidden");
+  } else {
+    isFxFocusToast.innerHTML = "No!..not a firefox focus browser";
+    isFxFocusToast.classList.remove("hidden");
+  }
+  setTimeout(() => {
+    isFxFocusToast.classList.add("hidden");
   }, 1000);
 }
 
 function detectBrowser() {
   let userAgent = navigator.userAgent;
-  let userAgentBlock = document.querySelector("#user-agent-code-block");
-  let mimeBlock = document.querySelector("#mimeTypes");
+  let userAgentBlock = document.querySelector("#userAgentBlock");
   let cookieEnabled = document.querySelector("#cookieEnabled");
-  let plugins = document.querySelector("#plugins");
+  let pdfViewer = document.querySelector("#pdfViewer");
+  let serviceWorker = document.querySelector("#serviceWorker");
   let copyAction = document.querySelector("#copyAction");
+  let isFxFocusBtn = document.querySelector("#isFxFocusBtn");
 
   copyAction.addEventListener("click", copyToClipBoard);
-  userAgentBlock.innerHTML = userAgent;
-  cookieEnabled.innerHTML = `pdf viewer enable: ${navigator}`;
-  plugins.innerHTML = `serviceWorker: ${"serviceWorker" in navigator ? "suppported" : "not"}`;
+  isFxFocusBtn.addEventListener("click", isFxFocusCheck);
 
-  //console.log("serviceWorker" in navigator ? "supo");
+  userAgentBlock.innerHTML = userAgent;
+  cookieEnabled.innerHTML = `Cookie Enabled: ${navigator.cookieEnabled}`;
+  pdfViewer.innerHTML = `PDF Viewer: ${navigator.cookieEnabled}`;
+  serviceWorker.innerHTML = `Service Worker: ${"serviceWorker" in navigator ? "suppported" : "not"}`;
 }
 
 window.onload = function (e) {
