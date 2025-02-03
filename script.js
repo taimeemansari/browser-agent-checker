9d59c75aba7e7222ee61c3a6942143eec1ef87f2
taimeem ansari <taimeemansari.n@gmail.com>
2025-02-03 18:12:20 +0530

    service worker check

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function detectBrowser() {
   userAgentBlock.innerHTML = userAgent;
   cookieEnabled.innerHTML = `Cookie Enabled: ${navigator.cookieEnabled}`;
   pdfViewer.innerHTML = `PDF Viewer: ${navigator.cookieEnabled}`;
-  serviceWorker.innerHTML = `Service Worker: ${"serviceWorker" in navigator ? "suppported" : "not"}`;
+  serviceWorker.innerHTML = `${"serviceWorker" in navigator}`;
 }
 
 window.onload = function (e) {
